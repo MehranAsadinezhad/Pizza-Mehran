7914e6cfc9d5279e75b6b4c5191c50bb0794a443
Mehran <mehranasadition@gmail.com>
2023-09-15 19:53:39 +0430

    SearchOrder is created

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="w-64 rounded-full bg-light px-5 py-2 font-vazir outline-none transition-all duration-200 placeholder:font-vazir focus:w-72 focus:ring-2 focus:ring-secondary"
+        placeholder="جست و جوی سفارش #"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      ></input>
+    </form>
+  );
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <header className="flex h-14 flex-row-reverse items-center justify-between bg-primary px-5">
-      <Link className="font-cody transition-all duration-200 hover:text-medium text-3xl font-bold tracking-widest text-light">
+      <Link className="font-cody text-3xl font-bold tracking-widest text-light transition-all duration-200 hover:text-medium">
         PIZZA MEHRAN
       </Link>
-      <input
-        className="w-64 bg-light rounded-full py-2 px-5 font-vazir outline-none transition-all duration-200 placeholder:font-vazir focus:w-72 focus:ring-2 focus:ring-secondary"
-        placeholder="جست و جوی سفارش #"
-      ></input>
+      <SearchOrder />
     </header>
   );
 }
