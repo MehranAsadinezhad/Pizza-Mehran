04433c21a2199fdea461a0f1c5e7789fcb808b91
Mehran <mehranasadition@gmail.com>
2023-09-15 11:18:25 +0430

    Loading is added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import Cart from "../features/cart/Cart";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader/>}
+      
       <Header />
       <div className="overflow-scroll">
         <main>
